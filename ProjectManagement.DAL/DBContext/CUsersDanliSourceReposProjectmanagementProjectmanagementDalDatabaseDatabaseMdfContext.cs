9698e10e77a58419572a69c86bbbc40ddbeb06d0
Daniel Lima <danielmlimag@gmail.com>
2023-05-04 20:51:05 -0300

    feat: add database

---
+++ ProjectManagement.DAL/DBContext/CUsersDanliSourceReposProjectmanagementProjectmanagementDalDatabaseDatabaseMdfContext.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectManagement.DAL.DBContext;
+
+public partial class CUsersDanliSourceReposProjectmanagementProjectmanagementDalDatabaseDatabaseMdfContext : DbContext
+{
+    public CUsersDanliSourceReposProjectmanagementProjectmanagementDalDatabaseDatabaseMdfContext()
+    {
+    }
+
+    public CUsersDanliSourceReposProjectmanagementProjectmanagementDalDatabaseDatabaseMdfContext(DbContextOptions<CUsersDanliSourceReposProjectmanagementProjectmanagementDalDatabaseDatabaseMdfContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Project> Projects { get; set; }
+
+    public virtual DbSet<Status> Statuses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\danli\\source\\repos\\ProjectManagement\\ProjectManagement.DAL\\database\\database.mdf;Integrated Security=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Project>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Project__3213E83FC209CB4B");
+
+            entity.ToTable("Project");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.FinishDate)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("finish_date");
+            entity.Property(e => e.IdStatus).HasColumnName("id_status");
+            entity.Property(e => e.ManagerName)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("manager_name");
+            entity.Property(e => e.ProjectName)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("project_name");
+            entity.Property(e => e.StartDate)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("start_date");
+            entity.Property(e => e.Summary)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("summary");
+
+            entity.HasOne(d => d.IdStatusNavigation).WithMany(p => p.Projects)
+                .HasForeignKey(d => d.IdStatus)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Project__id_stat__38996AB5");
+        });
+
+        modelBuilder.Entity<Status>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Status__3213E83F8F098038");
+
+            entity.ToTable("Status");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.Status1)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("status");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ProjectManagement.DAL/DBContext/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectManagement.DAL.DBContext;
+
+public partial class Status
+{
+    public int Id { get; set; }
+
+    public string Status1 { get; set; } = null!;
+
+    public virtual ICollection<Project> Projects { get; set; } = new List<Project>();
+}
